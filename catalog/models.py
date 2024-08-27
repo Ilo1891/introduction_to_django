5de8f9d54ca8688781044c9c732741d17de4b3fe
Кутибашвили Илья <generals22@mail.ru>
2024-08-27 20:34:38 +0300

    add media

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=64, verbose_name="name")
     description = models.CharField(max_length=64, verbose_name="description")
-    image = models.ImageField(upload_to="catalog", verbose_name="image")
+    image = models.ImageField(upload_to="media/catalog", verbose_name="image")
     price = models.IntegerField(verbose_name="price")
     created = models.DateField(verbose_name="created", auto_now_add=True)
     update = models.DateField(verbose_name="created", auto_now=True)
